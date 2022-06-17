2ce1f8a05c5a011cf9284af699beeed3e9ae3447
MicroSTM <MicroSTM>
2022-06-17 16:01:15 -0400

    init commit

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -1,4 +1,6 @@
 import random
+import numpy as np
+import json
 
 
 def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
@@ -39,39 +41,60 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
             'submitted_at': u'2017-03-20T19:22:03.880652Z'
         }
     """
+
+    print(kwargs["submission_metadata"])
+
+    with open(test_annotation_file, "r") as f:
+        GT = json.load(f)
+
+    with open(user_submission_file, "r") as f:
+        result = json.load(f)
+
+    scores = {}
+
+    for key, score in result.items():
+        trial_name = GT[key][0]
+        test_class = GT[key][1]
+
+        if trial_name not in scores:
+            scores[trial_name] = {}
+        scores[trial_name][test_class] = score
+
+    acc_1 = []
+    acc_2 = []
+    acc_3 = []
+    acc_4 = []
+    acc_all = []
+
+    for trial_name, ratings in scores.items():
+        if abs(ratings["expected"] - ratings["surprising"]) < 1e-6:
+            correct = 0.5
+        else:
+            if ratings["expected"] < ratings["surprising"]:
+                correct = 1
+            else:
+                correct = 0
+        if "scenario_1" in trial_name:
+            acc_1.append(correct)
+        if "scenario_2" in trial_name:
+            acc_2.append(correct)
+        if "scenario_3" in trial_name:
+            acc_3.append(correct)
+        if "scenario_4" in trial_name:
+            acc_4.append(correct)
+        acc_all.append(correct)
+
     output = {}
-    if phase_codename == "dev":
-        print("Evaluating for Dev Phase")
-        output["result"] = [
-            {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            }
-        ]
-        # To display the results in the result file
-        output["submission_result"] = output["result"][0]["train_split"]
-        print("Completed evaluation for Dev Phase")
-    elif phase_codename == "test":
+    if phase_codename == "test":
         print("Evaluating for Test Phase")
         output["result"] = [
             {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-            {
                 "test_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
+                    "Scneario 1": np.mean(acc_1),
+                    "Scneario 2": np.mean(acc_2),
+                    "Scneario 3": np.mean(acc_3),
+                    "Scneario 4": np.mean(acc_4),
+                    "All": np.mean(acc_all),
                 }
             },
         ]
