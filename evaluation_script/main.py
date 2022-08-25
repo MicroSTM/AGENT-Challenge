51b253bc73a984f34e7c6805d16d2d3558219174
MicroSTM <MicroSTM>
2022-08-25 13:52:17 -0400

    update

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -44,21 +44,21 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
 
     print(kwargs["submission_metadata"])
 
-    # with open(test_annotation_file, "r") as f:
-    #     GT = json.load(f)
+    with open(test_annotation_file, "r") as f:
+        GT = json.load(f)
 
-    # with open(user_submission_file, "r") as f:
-    #     result = json.load(f)
+    with open(user_submission_file, "r") as f:
+        result = json.load(f)
 
     scores = {}
 
-    # for key, score in result.items():
-    #     trial_name = GT[key][0]
-    #     test_class = GT[key][1]
+    for key, score in result.items():
+        trial_name = GT[key][0]
+        test_class = GT[key][1]
 
-    #     if trial_name not in scores:
-    #         scores[trial_name] = {}
-    #     scores[trial_name][test_class] = score
+        if trial_name not in scores:
+            scores[trial_name] = {}
+        scores[trial_name][test_class] = score
 
     acc_1 = []
     acc_2 = []
@@ -66,57 +66,39 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
     acc_4 = []
     acc_all = []
 
-    # for trial_name, ratings in scores.items():
-    #     if abs(ratings["expected"] - ratings["surprising"]) < 1e-6:
-    #         correct = 0.5
-    #     else:
-    #         if ratings["expected"] < ratings["surprising"]:
-    #             correct = 1
-    #         else:
-    #             correct = 0
-    #     if "scenario_1" in trial_name:
-    #         acc_1.append(correct)
-    #     if "scenario_2" in trial_name:
-    #         acc_2.append(correct)
-    #     if "scenario_3" in trial_name:
-    #         acc_3.append(correct)
-    #     if "scenario_4" in trial_name:
-    #         acc_4.append(correct)
-    #     acc_all.append(correct)
+    for trial_name, ratings in scores.items():
+        if abs(ratings["expected"] - ratings["surprising"]) < 1e-6:
+            correct = 0.5
+        else:
+            if ratings["expected"] < ratings["surprising"]:
+                correct = 1
+            else:
+                correct = 0
+        if "scenario_1" in trial_name:
+            acc_1.append(correct)
+        if "scenario_2" in trial_name:
+            acc_2.append(correct)
+        if "scenario_3" in trial_name:
+            acc_3.append(correct)
+        if "scenario_4" in trial_name:
+            acc_4.append(correct)
+        acc_all.append(correct)
 
     output = {}
-    # if phase_codename == "test":
-    #     print("Evaluating for Test Phase")
-    #     output["result"] = [
-    #         {
-    #             "test_split": {
-    #                 "Scneario 1": np.mean(acc_1),
-    #                 "Scneario 2": np.mean(acc_2),
-    #                 "Scneario 3": np.mean(acc_3),
-    #                 "Scneario 4": np.mean(acc_4),
-    #                 "All": np.mean(acc_all),
-    #             }
-    #         },
-    #     ]
-    #     # To display the results in the result file
-    #     output["submission_result"] = output["result"][0]
-    #     print("Completed evaluation for Test Phase")
-
     if phase_codename == "test":
         print("Evaluating for Test Phase")
         output["result"] = [
             {
                 "test_split": {
-                    "Scneario 1": 0,
-                    "Scneario 2": 0,
-                    "Scneario 3": 0,
-                    "Scneario 4": 0,
-                    "All": 0,
+                    "Scneario 1": np.mean(acc_1),
+                    "Scneario 2": np.mean(acc_2),
+                    "Scneario 3": np.mean(acc_3),
+                    "Scneario 4": np.mean(acc_4),
+                    "All": np.mean(acc_all),
                 }
             },
         ]
         # To display the results in the result file
         output["submission_result"] = output["result"][0]
         print("Completed evaluation for Test Phase")
-
     return output
